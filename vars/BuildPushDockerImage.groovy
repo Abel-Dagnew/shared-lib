46c7d922a86fe7339fa413682c7683398f8deeae
Abelvm <Abeltesfaye111@gmail.com>
2025-02-04 13:19:08 +0000

    updated script for docker builld

--- vars/BuildPushDockerImage.groovy
+++ vars/BuildPushDockerImage.groovy
@@ -2,40 +2,16 @@
 def call(ACR_NAME, DOCKER_IMAGE_NAME, ACR_LOGIN_SERVER) {
     // This function will contain only the logic, and stages will be called from the pipeline
     return {
-        stage('Build Docker Image') {
-            steps {
                 script {
                     // Build Docker image
                     sh "docker build -t ${ACR_NAME}/${DOCKER_IMAGE_NAME}:latest ."
-                }
-            }
-        }
-
-        stage('List Docker Images') {
-            steps {
-                script {
                     // List Docker images to verify the image tag exists
                     sh 'docker images'
-                }
-            }
-        }
-
-        stage('Tag Docker Image') {
-            steps {
-                script {
                     // Tag the Docker image for Azure Container Registry
                     sh "docker tag ${ACR_NAME}/${DOCKER_IMAGE_NAME}:latest ${ACR_LOGIN_SERVER}/${DOCKER_IMAGE_NAME}:latest"
-                }
-            }
-        }
-
-        stage('Push Docker Image to ACR') {
-            steps {
-                script {
                     // Push the Docker image to Azure Container Registry
                     sh "docker push ${ACR_LOGIN_SERVER}/${DOCKER_IMAGE_NAME}:latest"
                 }
-            }
-        }
+        
     }
 }
